7d09a73da1686ff9ca7f15cf605265139b8d20ee
Kim Hongil <hoil20707@gmail.com>
2022-06-14 20:49:34 +0900

    선기+홍일 병합 버그 수정

--- src/main/java/com/spring/ex/controller/MainPageController.java
+++ src/main/java/com/spring/ex/controller/MainPageController.java
@@ -1,6 +1,5 @@
 package com.spring.ex.controller;
 
-import java.util.HashMap;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -12,13 +11,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.spring.ex.dto.CourseDTO;
-import com.spring.ex.dto.CourseReplyDTO;
 import com.spring.ex.dto.TagDTO;
+import com.spring.ex.dto.course.CourseDTO;
+import com.spring.ex.dto.course.CourseReplyDTO;
 import com.spring.ex.service.CourseBestService;
-import com.spring.ex.service.CourseService;
 import com.spring.ex.service.LiveCourseReplyService;
-import com.spring.ex.service.PagingService;
 import com.spring.ex.service.TagService;
 
 @Controller
--- src/main/java/com/spring/ex/dao/CourseBestDAO.java
+++ src/main/java/com/spring/ex/dao/CourseBestDAO.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
-import com.spring.ex.dto.CourseDTO;
+import com.spring.ex.dto.course.CourseDTO;
 
 @Repository
 public interface CourseBestDAO {
